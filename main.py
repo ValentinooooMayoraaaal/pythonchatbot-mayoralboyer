88de4d2ccd1083e3417da0fcda0f34af6565b938
Valentino <valkoush@gmail.com>
2023-12-07 09:53:05 +0100

    Commit_minuscula

--- main.py
+++ main.py
@@ -48,8 +48,10 @@ def cleaned(file):
             for c in lines:
                 if 65 <= ord(c) <= 90:
                     c = chr(ord(c) + 32)
-                if not (97 <= ord(c) <= 122 or c == "é" or c == "É" or c == "ç" or c == "à" or c == " "):
+                if not (97 <= ord(c) <= 122 or c == "é" or c == "É" or c == "ç" or c == "à" or c == " " or c == "-" or c == "'"):
                     c = ""
+                if c == "-" or c == "'":
+                    c = " "
                 cleaned_content += c
             speech.append(cleaned_content)
         return speech
