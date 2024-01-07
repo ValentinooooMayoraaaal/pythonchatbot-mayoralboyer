63d4977016957f1b6b3ba405cd35794459d64981
Tom Boyer <tomboyer.tb94@gmail.com>
2024-01-07 13:11:50 +0100

    Menu

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 
 from fonctions import list_of_files, cleaned, calcule_tf_idf, matrice_vec, inversion_matrice, useless, higher_tf_idf, list_speech
 #Consigne : afficher le mot le plus répété chez Chirac
-#list_most_repeated_words = higher_tf_idf("Speeches")
-#print(list_most_repeated_words[0])
-#print(list_most_repeated_words[1])
+def higher_Chirac():
+    list_most_repeated_words = higher_tf_idf("Speeches")
+    return(list_most_repeated_words[0],list_most_repeated_words[1])
+
 
 #Consigne : afficher qui a parlé de la nation et combien de fois
-#for file in list_speech:
-    #with open("Speeches/"+file.split(".")[0]+"-cleaned.txt", 'r', encoding='utf-8') as f1:
-        #content = f1.read()
-        #words = content.split()
-        #cpt = 0
-        #for word in words:
-            #if word == "nation":
-                #verif = True
-                #cpt += 1
-        #if verif == True:
-            #print("Il y a ",cpt," fois nation dans ", file)
+def nation_repetition():
+    for file in list_speech:
+        with open("Speeches/"+file.split(".")[0]+"-cleaned.txt", 'r', encoding='utf-8') as f1:
+            content = f1.read()
+            words = content.split()
+            cpt = 0
+            for word in words:
+                if word == "nation":
+                    verif = True
+                    cpt += 1
+            if verif == True:
+                print("Il y a ",cpt," fois nation dans ", file)
 
 #Consigne : Indiquer le premier président à parler du climat et/ou de l’écologie
 def Qui_parlait_écologie():
@@ -54,7 +56,24 @@ def mots_repetes_par_tous():
             mots_importants.append(mots_repaire[i])
     return mots_importants
 
+def menu():
+    print("Bienvenue dans le menu.")
+    print("Ce programme vous permet de recueillir des informations à partir des différents textes de nominations des présidents. En fonction de vos envies, sélectionnez le numéro de votre choix.")
+    print("Tapez 1 pour connaître la liste des mots les moins importants dans le corpus de documents.")
+    print("Tapez 2 pour connaître les mots les plus répétés pour chacun des discours.")
+    print("Tapez 3 pour connaître le mot le plus répété par Jacques Chirac.")
+    print("Tapez 4 pour connaître le(s) nom(s) du (des) président(s) qui a (ont) parlé de la « Nation » et celui qui l’a répété le plus de fois.")
+    print("Tapez 5 pour connaître le premier président à parler du climat et de l’écologie dans ses discours.")
+    print("Tapez 6 pour connaître quels sont les mots importants que tous les présidents ont évoqués.")
+    x = int(input("A quel connaissance souhaitez-vous accéder ?"))
+    if x == 1: print(useless("Speeches"))
+    if x == 2 : print(higher_tf_idf("Speeches"))
+    if x == 3 : print(higher_Chirac())
+    if x == 4 : print(nation_repetition())
+    if x == 5 : print(Qui_parlait_écologie())
+    if x == 6 : print(mots_repetes_par_tous())
 
+menu()
 
 
 
